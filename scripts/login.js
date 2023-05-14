a865daeac3825766da6a625ec5edd18441984833
lucascampos04 <camposdlucasoli@gmail.com>
2023-05-13 22:49:20 -0300

    add OliveiraWeb

---
+++ scripts/login.js
@@ -0,0 +1,38 @@
+function login() {
+    const user = document.getElementById('usuario').value
+    const senha = document.getElementById('senha').value
+  
+    if (user == 'admin' && senha == '123') {
+      alert("Login efetuado com sucesso")
+      window.location.href = '/OliveiraWeb/perfil-admin.html'
+      console.log("Login efetuado com sucesso \n Seja bem vindo" + user)
+    } else {
+      alert("Login ou senha incorretos")
+      console.log("Login ou senha incorretos")
+    }
+  }
+  
+  document.addEventListener('keypress', function (event) {
+    if (event.key === 'Enter') {
+      login();
+    }
+  });
+  
+  function toggleSenhaVisibility() {
+    var senhaInput = document.getElementById("senha");
+    var toggleButton = document.getElementById("toggle-senha");
+  
+    if (senhaInput.type === "password") {
+      senhaInput.type = "text";
+      toggleButton.classList.add("visible");
+    } else {
+      senhaInput.type = "password";
+      toggleButton.classList.remove("visible");
+    }
+  }
+  
+  
+  
+  
+  
+  
