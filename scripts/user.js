935ec67c4bee3b1cbc005c81635ccff6548d5ea9
Lucascampos04 <camposdlucasoli@gmail.com>
2023-05-29 15:30:15 -0800

    commit 7.0
    Começo do lobby

--- scripts/user.js
+++
@@ -1,6 +0,0 @@
-const menuIcon = document.querySelector('.menu a');
-const menu = document.querySelector('.menu');
-
-menuIcon.addEventListener('click', function() {
-  menu.classList.toggle('open');
-});
